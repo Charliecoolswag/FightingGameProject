7f6a5129398144dc6a3ee7a0d9f230bf3740f477
Charliecoolswag <charliecoolswag@gmail.com>
2024-03-17 21:14:39 +0100

    Healthbar

--- Assets/Scipts/PlayerMovement.cs
+++ Assets/Scipts/PlayerMovement.cs
@@ -5,16 +5,42 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
-    public Animator animator; 
+    public Animator animator;
 
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public Healthbar healthBar;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+           // animator.SetTrigger("Punch");
+            TakeDamage(5);
+        }
+
+        
+
         animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
 
         Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
 
         transform.position = transform.position + horizontal * Time.deltaTime;
     }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        healthBar.SetHealth(currentHealth);
+    }
+
 }
