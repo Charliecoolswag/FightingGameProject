fc4766d6b7cd919f30580c90e55447d1cc80baee
Charliecoolswag <charliecoolswag@gmail.com>
2024-06-04 16:03:29 +0200

    wintext and cleanup

--- Assets/Scipts/HealthbarP2.cs
+++ Assets/Scipts/HealthbarP2.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Healthbar : MonoBehaviour
+public class HealthbarP2 : MonoBehaviour
 {
 
     public Slider slider;
