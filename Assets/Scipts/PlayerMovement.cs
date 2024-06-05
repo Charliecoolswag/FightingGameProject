8af5a76315e13f49dcbdbf9bdec508d9216ffd24
Charliecoolswag <charliecoolswag@gmail.com>
2024-06-06 00:17:06 +0200

    soundeffects

--- Assets/Scipts/PlayerMovement.cs
+++ Assets/Scipts/PlayerMovement.cs
@@ -2,14 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.UI;
-using TMPro;
+using UnityEngine.SceneManagement;
+using TMPro;  // Add this namespace for TextMeshPro
 
 public class PlayerMovement : MonoBehaviour
 {
     public Animator animator;
 
     public GameObject winTextObject;
+    public TextMeshProUGUI countdownText; // Add this line to reference the TextMeshPro UI component
 
     public HealthbarP1 healthBarP1;
     public int P1MaxHealth = 100;
@@ -23,12 +24,14 @@ public class PlayerMovement : MonoBehaviour
     private bool struck;
     private bool dead;
 
+    private AudioSource source;
+    public AudioClip hitClip;
+    public AudioClip youWin;
+    public AudioClip impactClip;
 
     public GameObject attackPoint;
     public float radius;
     public LayerMask enemies;
-
-
 
     [SerializeField]
     private float movementSpeed;
@@ -40,7 +43,6 @@ public class PlayerMovement : MonoBehaviour
 
     [SerializeField]
     private float groundRadius;
-
 
     [SerializeField]
     private LayerMask whatIsGround;
@@ -54,37 +56,35 @@ public class PlayerMovement : MonoBehaviour
 
     private Rigidbody2D myRigidbody;
 
-
     [SerializeField] PlayerInput playerInput;
 
-
-
-
+    private bool canProcessInput = true; // Add this line to control input processing
 
     private void Start()
     {
         winTextObject.SetActive(false);
 
-
         facingRight = true;
         P1CurrentHealth = P1MaxHealth;
         P2CurrentHealth = P2MaxHealth;
         healthBarP2.SetMaxHealth(P2MaxHealth);
-       
 
-        myRigidbody= GetComponent<Rigidbody2D>();
-        animator=GetComponent<Animator>();
+        myRigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
         playerInput = GetComponent<PlayerInput>();
+        source = GetComponent<AudioSource>();
+
+        countdownText.gameObject.SetActive(false);  // Initially hide the countdown text
     }
 
     private bool IsGrounded()
     {
-        if (myRigidbody.velocity.y <=0)
+        if (myRigidbody.velocity.y <= 0)
         {
             foreach (Transform point in groundPoints)
             {
-                Collider2D[] colliders = Physics2D.OverlapCircleAll(point.position,groundRadius,whatIsGround);
-                for (int i=0; i < colliders.Length; i++)
+                Collider2D[] colliders = Physics2D.OverlapCircleAll(point.position, groundRadius, whatIsGround);
+                for (int i = 0; i < colliders.Length; i++)
                 {
                     if (colliders[i].gameObject != gameObject)
                     {
@@ -92,36 +92,33 @@ public class PlayerMovement : MonoBehaviour
                     }
                 }
             }
-            
         }
         return false;
     }
 
-
-    
-
-
-    // Update is called once per frame ------------------------------------------------------------------------
     void Update()
     {
+        if (!canProcessInput) return; // Add this line to skip input processing if input is disabled
 
         HandleInput();
-        //HandleAttacks();
         HandleBlocks();
 
-
-       if (P2CurrentHealth <=0 && !win)
+        if (P2CurrentHealth <= 0 && !win)
         {
             win = true;
             SetWin();
             winTextObject.SetActive(true);
-        }
 
+            // Disable input
+            canProcessInput = false;
+
+            // Start the coroutine to reload the scene after a delay
+            StartCoroutine(ReloadSceneAfterDelay(10f));  // Delay set to 10 seconds
+        }
 
         float horizontal = playerInput.actions["Move"].ReadValue<Vector2>().x;
         bool jump = playerInput.actions["Jump"].WasPerformedThisFrame();
         bool attack = playerInput.actions["Attack"].WasPerformedThisFrame();
-
 
         if (isGrounded && jump)
         {
@@ -130,69 +127,64 @@ public class PlayerMovement : MonoBehaviour
         }
         animator.SetFloat("speed", Mathf.Abs(horizontal));
 
-
-
         isGrounded = IsGrounded();
         HandleMovement(horizontal);
         Flip(horizontal);
 
-
-        
-
         if (attack)
         {
             animator.SetBool("isAttacking", true);
+            source.PlayOneShot(hitClip);
         }
-
     }
 
+    private IEnumerator ReloadSceneAfterDelay(float delay)
+    {
+        countdownText.gameObject.SetActive(true);  // Show the countdown text
 
+        int remainingTime = Mathf.CeilToInt(delay);
+        while (remainingTime > 0)
+        {
+            countdownText.text = $"Reloading in {remainingTime} seconds";
+            yield return new WaitForSeconds(1f);  // Update the text every second
+            remainingTime--;
+        }
 
+        countdownText.text = "Reloading now...";
+        yield return new WaitForSeconds(1f);  // Wait a bit before reloading
 
+        Scene currentScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(currentScene.name);
+    }
 
     public void attack()
     {
         Collider2D[] P2Damage = Physics2D.OverlapCircleAll(attackPoint.transform.position, radius, enemies);
-        foreach(Collider2D enemyGameobject in P2Damage)
+        foreach (Collider2D enemyGameobject in P2Damage)
         {
             Debug.Log("Hit enemy");
             TakeDamageP2(10);
-            
+            source.PlayOneShot(impactClip);
         }
-
 
         Collider2D[] P1Damage = Physics2D.OverlapCircleAll(attackPoint.transform.position, radius, enemies);
         foreach (Collider2D enemyGameobject in P1Damage)
         {
             Debug.Log("Hit enemy");
             TakeDamageP1(10);
-
         }
-
-
     }
 
-    /*
-    public void isStruck()
-    {
-        animator.SetBool("struck", true);
-    }
-    */
-
-    
     public void endAttack()
     {
         animator.SetBool("isAttacking", false);
     }
-    
 
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(attackPoint.transform.position, radius);
     }
-
-
 
     private void Flip(float horizontal)
     {
@@ -204,16 +196,11 @@ public class PlayerMovement : MonoBehaviour
             theScale.x *= -1;
 
             transform.localScale = theScale;
-        } 
+        }
     }
 
-    // movement 
-        private void HandleMovement(float horizontal)
+    private void HandleMovement(float horizontal)
     {
-        
-
-   
-        //  myRigidbody.velocity = new Vector2(horizontal * movementSpeed, myRigidbody.velocity.y); 
         if (!animator.GetCurrentAnimatorStateInfo(0).IsTag("isAttacking"))
         {
             myRigidbody.velocity = new Vector2(horizontal * movementSpeed, myRigidbody.velocity.y);
@@ -222,26 +209,7 @@ public class PlayerMovement : MonoBehaviour
         {
             myRigidbody.velocity = new Vector2(0, 0);
         }
-
-        
-        
-
-        
-
-
-        /*
-        if (struck && !this.animator.GetCurrentAnimatorStateInfo(0).IsName("Struck"))
-        {
-            animator.SetBool("struck", true);
-            TakeDamage(5);
-        } else if (!this.animator.GetCurrentAnimatorStateInfo(0).IsName("Struck"))
-        {
-            animator.SetBool("struck", false);
-        }
-        */
     }
-
-
 
     private void HandleBlocks()
     {
@@ -252,60 +220,51 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    private void HandleInput() 
+    private void HandleInput()
     {
-      
         if (Input.GetKeyDown(KeyCode.G))
         {
-            block = true; 
-        }  else { 
-            block = false; }
+            block = true;
+        }
+        else
+        {
+            block = false;
+        }
 
         if (Input.GetKeyDown(KeyCode.R))
         {
             struck = true;
-        } else  {
+        }
+        else
+        {
             struck = false;
         }
-
     }
-
 
     private void ResetValues()
     {
-        block= false;
-        win= false;
-        struck= false;
+        block = false;
+        win = false;
+        struck = false;
         jump = false;
-        
     }
 
     void TakeDamageP1(int damage)
     {
         P1CurrentHealth -= damage;
-
         healthBarP1.SetHealth(P1CurrentHealth);
     }
-
 
     void TakeDamageP2(int damage)
     {
         P2CurrentHealth -= damage;
-
         healthBarP2.SetHealth(P2CurrentHealth);
     }
-
-
-
-    
-    
 
     void SetWin()
     {
         animator.SetBool("win", true);
         Debug.Log("Win animation triggered in PlayerMovement");
-       
+        source.PlayOneShot(youWin);
     }
-
-
 }
